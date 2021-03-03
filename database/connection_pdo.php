76d1b8978562e2ee5b77a532f17994a49515d810
Laciface <csomali18@gmail.com>
2021-03-03 20:13:32 +0100

    feat: build the PDO database connection with connectToDatabase function

--- database/connection_pdo.php
+++ database/connection_pdo.php
@@ -1,1 +1,21 @@
 <?php
+class DBConnection
+{
+    public static function connectToDatabase(): ?PDO
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password = "passwordsql";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=back-to-the-future", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch
+        (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+}
+?>
