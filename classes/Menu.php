3b8258a15159ca3936dcfcebcedf8eb45e2e2c44
Laciface <csomali18@gmail.com>
2021-03-04 11:37:25 +0100

    feat: Menu.php functions declaration done

---
+++ classes/Menu.php
@@ -0,0 +1,40 @@
+<?php
+
+
+use app\models\Question;
+
+class Menu
+{
+    private ?PDO $pdo;
+
+    /**
+     * Model constructor.
+     */
+    public function __construct()
+    {
+        $this->pdo = DBConnection::connectToDatabase();
+    }
+    private $menu = array();
+    function Menu()
+    {
+        $pdo = $this->pdo;
+        $sql = 'SELECT * FROM `MENU` ORDER BY `POSITION` ASC';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        foreach ($result as $row) {
+            array_push($this->menu, $row);
+        }
+    }
+
+    function render()
+    {
+        $menuContent = "";
+        for ($i = 0; $i < count($this->menu); $i++) {
+            $menuContent = $menuContent . sprintf('<a href="%s">', $this->menu[$i]["FILE"]) . $this->menu[$i]["LABEL"] . "</a>&nbsp;";
+        }
+
+        return $menuContent;
+    }
+}
